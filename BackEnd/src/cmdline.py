f6320e3439180ae21a677a4e68214da9ff9f7ea1
Scott Gibb <Kyle.Lawson@babcockdev.onmicrosoft.com>
2023-03-31 20:25:04 +0100

    updating cmdline

--- BackEnd/src/cmdline.py
+++ BackEnd/src/cmdline.py
@@ -4,4 +4,8 @@ from fan_controller import FanController
 controller = FanController(37,35)
 
 controller.duty_cycle = 50
-print(controller.rpm)
+
+while True:
+    new_duty_cycle = int(input("Enter new duty cycle: "))
+    print("Setting duty cycle to " + str(new_duty_cycle))
+    print("Current RPM: " + str(controller.rpm))
