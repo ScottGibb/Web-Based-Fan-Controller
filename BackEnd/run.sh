688558cbe756bb4d5a8f076e7c6257ceb30ecb72
Scott Gibb <sgibb@witekio.com>
2023-03-31 10:23:50 +0100

    Fixing syntax

--- BackEnd/run.sh
+++ BackEnd/run.sh
@@ -8,7 +8,7 @@ docker build -t pythonwebapp-backend .
 
 # Run the Docker Image in the background
 
-if[$# -eq 0]; then
+if [$# -eq 0]; then
     docker run -p 5000:5000 -d pythonwebapp-backend
 else
     docker run -p 5000:5000 -d -e MOCK=$1 pythonwebapp-backend 
